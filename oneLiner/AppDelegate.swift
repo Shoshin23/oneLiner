388b79172229c8b1d59fb8dcf4efc62aee732c2b
Karthik Kannan <karthikk.314@gmail.com>
2016-06-17 21:57:23 +0530

    - Fixed code issue with App Delegate. Able to extract APS payload to send to controller. More elegant. - Removed some redundant code whenever I saw it. More systematic erasure will flow soon.

--- oneLiner/AppDelegate.swift
+++ oneLiner/AppDelegate.swift
@@ -17,7 +17,24 @@ import FirebaseMessaging
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    func connectToFcm() {
+        FIRMessaging.messaging().connectWithCompletion { (error) in
+            if (error != nil) {
+                print("Unable to connect with FCM. \(error)")
+            } else {
+                print("Connected to FCM.")
+            }
+        }
+    }
 
+    func tokenRefreshNotificaiton(notification: NSNotification) {
+        let refreshedToken = FIRInstanceID.instanceID().token()!
+        print("InstanceID token: \(refreshedToken)")
+        
+        // Connect to FCM since connection may have failed when attempted before having a token.
+        connectToFcm()
+    }
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,42 +48,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // Add observer for InstanceID token refresh callback.
         
-        func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject],
-                         fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
-            
-            // Print message ID.
-            print("Message ID: \(userInfo["gcm.message_id"]!)")
-            
-            // Print full message.
-            print("%@", userInfo)
-        }
-        func tokenRefreshNotificaiton(notification: NSNotification) {
-            let refreshedToken = FIRInstanceID.instanceID().token()!
-            print("InstanceID token: \(refreshedToken)")
-            
-            // Connect to FCM since connection may have failed when attempted before having a token.
-            connectToFcm()
-        }
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.tokenRefreshNotificaiton),name: kFIRInstanceIDTokenRefreshNotification, object: nil)
         
-        func connectToFcm() {
-            FIRMessaging.messaging().connectWithCompletion { (error) in
-                if (error != nil) {
-                    print("Unable to connect with FCM. \(error)")
-                } else {
-                    print("Connected to FCM.")
-                }
-            }
-        }
-
-        func applicationDidBecomeActive(application: UIApplication) {
-            connectToFcm()
-        }
-        
-        func applicationDidEnterBackground(application: UIApplication) {
-            FIRMessaging.messaging().disconnect()
-            print("Disconnected from FCM.")
-        }
-        UINavigationBar.appearance().barTintColor = UIColor(red:0.957, green:0.925, blue:0.055, alpha:1.0)
+                UINavigationBar.appearance().barTintColor = UIColor(red:0.957, green:0.925, blue:0.055, alpha:1.0)
         UINavigationBar.appearance().tintColor = UIColor.whiteColor()
         if let barFont = UIFont(name: "Avenir-Light", size: 24.0) {
             UINavigationBar.appearance().titleTextAttributes =
@@ -80,6 +64,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject],
+                     fetchCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+        
+        // Print message ID.
+        print("Message ID: \(userInfo["gcm.message_id"]!)")
+        
+        // Print full message.
+        print("%@", userInfo)
+        
+        if let aps = userInfo["aps"] as? NSDictionary {
+            if let alert = aps["alert"] as? NSDictionary {
+                if let message = alert["message"] as? NSString {
+                    print(message)
+                }
+            } else if let alert = aps["alert"] as? NSString {
+                print(alert)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        connectToFcm()
+    }
+    
+    func applicationDidEnterBackground(application: UIApplication) {
+        FIRMessaging.messaging().disconnect()
+        print("Disconnected from FCM.")
+    }
+
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -92,10 +110,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
@@ -106,41 +120,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
        // print(notificationSettings.types.rawValue)
     }
     
-    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
-        
-        if (application.applicationState == UIApplicationState.Inactive) {
-            print("You're here from the push notification, aren't you?")
-            self.redirectToView(notification.userInfo)
-        }
-    }
     
-        func redirectToView(userInfo:[NSObject: AnyObject]!) {
-            
-            let redirectViewController:UIViewController!
-        if userInfo != nil {
-            if let pageType = userInfo!["TYPE"] {
-                if pageType as! String == "SharePage" {
-                    redirectViewController = ShareViewController()
-                
-                }
-                
-//                if redirectViewController != nil {
-//                    if self.window != nil && self.window?.rootViewController != nil {
-                       let rootVC = self.window?.rootViewController as! UINavigationController
-//                        
-                       let mainStoryboard:UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                        let shareVC = mainStoryboard.instantiateViewControllerWithIdentifier("ShareViewController") as! ShareViewController
-                
-                        rootVC.pushViewController(shareVC, animated: true)
-//                        if rootVC is UINavigationController {
-//                            (rootVC as! UINavigationController).pushViewController(redirectViewController, animated: true)
-//                        } else {
-//                            rootVC?.presentViewController(redirectViewController, animated: false, completion: nil)
-//                        }
-                    }
-                }
-                
-            }
         }
 
     
--- oneLiner/oneLinerDisplay.swift
+++ oneLiner/oneLinerDisplay.swift
@@ -22,62 +22,6 @@ class oneLinerDisplay: UITableViewController {
     let oneLiners = Topics.oneLiners
     
     
-    //Flags
-    var isDataFetched:Bool = false      //Check if data was fetched before or not.
-
-    
-
-    //Methods
-    
-    
-//    //Schedule Notifications based on the time of day. 8 AM in the morning and 6 in the evening.
-//    func scheduleNotifications(timeOfDay:String, alertTitle:String, alertBody:String) {
-//        
-//        
-//        let calendar:NSCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-//        var dateFire = NSDate()
-//        
-//        var fireComponents = calendar.components([NSCalendarUnit.Day, .Month, .Year, .Hour, .Minute],fromDate:dateFire)
-//        
-//        if(timeOfDay == "M") {
-//        
-//        if (fireComponents.hour >= 8) {
-//            
-//            dateFire = dateFire.dateByAddingTimeInterval(86400)
-//            fireComponents = calendar.components([NSCalendarUnit.Day, .Month, .Year, .Hour, .Minute],fromDate:dateFire)
-//            
-//        }
-//        
-//        fireComponents.hour = 8
-//        fireComponents.minute = 0
-//        }
-//        
-//        else if(timeOfDay == "E") {
-//            
-//        if (fireComponents.hour >= 19) {
-//            
-//            dateFire = dateFire.dateByAddingTimeInterval(86400)
-//            fireComponents = calendar.components([NSCalendarUnit.Day, .Month, .Year, .Hour, .Minute],fromDate:dateFire)
-//            
-//        }
-//        
-//        fireComponents.hour = 18
-//        fireComponents.minute = 00
-//    }
-//    
-//        dateFire = calendar.dateFromComponents(fireComponents)!
-//        
-//        let localNotification = UILocalNotification()
-//        localNotification.fireDate = dateFire
-//        localNotification.alertTitle = alertTitle
-//        localNotification.alertBody = alertBody
-////        localNotification.repeatInterval = NSCalendarUnit.Day
-//        localNotification.userInfo = ["TYPE":"SharePage"]
-//        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
-//    }
-    
-
-    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
         
@@ -89,14 +33,10 @@ class oneLinerDisplay: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.tableView.delegate = self
-        
         self.tableView.allowsMultipleSelection = false
-        //print(Topics.oneLiners)
         
-        
-       
+        //check for network connection. Else throw an error. 
         
         
     }
@@ -136,22 +76,6 @@ class oneLinerDisplay: UITableViewController {
 
         
         cell?.accessoryType = .Checkmark
-       
-        // fetch data from Firebase as soon as someone picks a topic and store it locally using Realm?
-
-//        let postRef = DataService.ds.REF_POSTS.childByAppendingPath(Topics.oneLiners[optionChecked!])
-//        
-//        postRef.observeEventType(.Value, withBlock: { snapshot in
-//            
-//        //print(snapshot.value)
-//            let enumerator = snapshot.children
-//            while let rest = enumerator.nextObject() as? FDataSnapshot {
-//                                print(rest.value)
-//               
-//                
-//            }
-//        
-//        })
         
         FIRMessaging.messaging().subscribeToTopic("/topics/\(self.oneLiners[optionChecked!])")
         print("Subscribed to the topic. Send me PNs?")
