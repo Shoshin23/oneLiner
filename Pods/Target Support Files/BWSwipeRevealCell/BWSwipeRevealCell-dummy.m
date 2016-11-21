60daed973af61e2fe76f6f40cfc28d06685e2214
Karthik Kannan <karthikk.314@gmail.com>
2016-11-21 16:36:21 +0530

    pod updates.

--- Pods/Target Support Files/BWSwipeRevealCell/BWSwipeRevealCell-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BWSwipeRevealCell : NSObject
-@end
-@implementation PodsDummy_BWSwipeRevealCell
-@end
--- Pods/Target Support Files/BWSwipeRevealCell/BWSwipeRevealCell-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "BWSwipeRevealCell.h"
-
-FOUNDATION_EXPORT double BWSwipeRevealCellVersionNumber;
-FOUNDATION_EXPORT const unsigned char BWSwipeRevealCellVersionString[];
-
